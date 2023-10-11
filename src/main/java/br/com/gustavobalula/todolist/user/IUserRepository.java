6d1c6bff6e4bbf4b83e974f5cd6881776dbb49f0
Balula12 <gugabasigb2@gmail.com>
2023-10-11 17:44:09 -0300

    feat: implementando restrições de apenas um usúario por username

--- src/main/java/br/com/gustavobalula/todolist/user/IUserRepository.java
+++ src/main/java/br/com/gustavobalula/todolist/user/IUserRepository.java
@@ -3,7 +3,9 @@ package br.com.gustavobalula.todolist.user;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface IUserRepository extends JpaRepository<UserModel, UUID>{
-    
+    UserModel findByUsername(String username);
 }
--- src/main/java/br/com/gustavobalula/todolist/user/UserController.java
+++ src/main/java/br/com/gustavobalula/todolist/user/UserController.java
@@ -2,6 +2,8 @@ package br.com.gustavobalula.todolist.user;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,16 @@ public class UserController {
     private IUserRepository userRepository;
 
 @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            System.out.println("Usúario Já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usúario já existe");
+        };
+
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     
     }
 }
