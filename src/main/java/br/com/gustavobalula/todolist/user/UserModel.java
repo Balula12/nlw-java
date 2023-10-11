dfaf6f27da40044ff8f52e49f48b0c7bdca5c985
Balula12 <gugabasigb2@gmail.com>
2023-10-11 16:33:51 -0300

    Feat: Estrutura do Banco de Dados

--- src/main/java/br/com/gustavobalula/todolist/user/UserModel.java
+++ src/main/java/br/com/gustavobalula/todolist/user/UserModel.java
@@ -1,39 +1,12 @@
 package br.com.gustavobalula.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     
-   public String username;
-   public String name;
-   public String password;
+   private String username;
+   private String name;
+   private String password;
 
-
-   //getters setters
-   
-   // Username
-   public void setUsername(String username) {
-       this.username = username;
-   }
-
-   public String getUsername() {
-       return username;
-   }
-
-   // Name
-   public void setName(String name) {
-       this.name = name;
-   }
-
-   public String getName() {
-       return name;
-   }
-   
-
-   // Password
-   public void setPassword(String password) {
-       this.password = password;
-   }
-
-   public String getPassword() {
-       return password;
-   }
 }
