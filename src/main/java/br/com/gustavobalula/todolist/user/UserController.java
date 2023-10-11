f77aba0015c0c92af4a93513a46f355ef5077b10
Balula12 <gugabasigb2@gmail.com>
2023-10-11 17:22:26 -0300

    feat: first user na tabela de dados

--- src/main/java/br/com/gustavobalula/todolist/user/UserController.java
+++ src/main/java/br/com/gustavobalula/todolist/user/UserController.java
@@ -1,6 +1,7 @@
 package br.com.gustavobalula.todolist.user;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
 @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel){
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
+    
     }
 }
