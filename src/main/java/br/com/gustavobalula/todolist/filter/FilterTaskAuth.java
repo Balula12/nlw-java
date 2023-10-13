3c88fdf318460dd588cff2f27ab1ebffdba22206
Balula12 <gugabasigb2@gmail.com>
2023-10-13 17:26:36 -0300

    feat: validação de tarefa e tempo de tarefa

--- src/main/java/br/com/gustavobalula/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/gustavobalula/todolist/filter/FilterTaskAuth.java
@@ -25,6 +25,9 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain  filterChain)
             throws ServletException, IOException {
 
+                var servletPath = request.getServletPath();
+
+                if(servletPath.equals("/tasks/")){
                 // Pegar a autenticação (login, senha)
                var authorization = request.getHeader("Authorization");
 
@@ -53,12 +56,18 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                 //validar senha
                 var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
                 if(passwordVerify.verified){
+                    request.setAttribute("idUser", user.getId());
                     filterChain.doFilter(request, response);
                 } else {
                     response.sendError(401);
                 }
+            
+            } else {
+                filterChain.doFilter(request, response);
+            };
+
                
-    }
+        }
 
    
 
--- src/main/java/br/com/gustavobalula/todolist/task/TaskController.java
+++ src/main/java/br/com/gustavobalula/todolist/task/TaskController.java
@@ -1,10 +1,19 @@
 package br.com.gustavobalula.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,11 +23,29 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create (@RequestBody TaskModel taskModel){
-        System.out.println("Chegou no Controller");
+    public ResponseEntity create (@RequestBody TaskModel taskModel, HttpServletRequest request){
+        System.out.println("Chegou no Controller" + request.getAttribute("idUser"));
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID)idUser);
+
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt()) ){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A Data de início deve ser maior que a data atual!");
+
+        }
+
+         if (taskModel.getStartAt().isAfter(taskModel.getEndAt()) ){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A Data de início deve menor do que a data de término!");
+         }
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.OK).body(task);
 
 
+    }
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+        return tasks;
     }
 }
